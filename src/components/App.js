f5b9a857fd3fae64ca13572d8a4208fccfad0ccf
Francesca Bentin <francescabentin@gmail.com>
2023-03-21 19:54:44 +0100

    add and install react kit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+/* SECCIÓN DE IMPORT */
+
+// - De React
+// - Nuestros
+// - Sass
+import '../styles/App.scss';
+// - Imágenes
+
+/* SECCIÓN DEL COMPONENTE */
+function App() {
+  /* VARIABLES ESTADO (DATOS) */
+
+  /* EFECTOS (código cuando carga la página) */
+
+  /* FUNCIONES HANDLER */
+
+  /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
+
+  /* HTML */
+  return <div className="App">{/* Aquí va el HTML */}</div>;
+}
+
+/* PROP-TYPES */
+
+/* EXPORT DEL COMPONENTE */
+export default App;
